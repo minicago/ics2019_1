f33751541464c5d62f806dbd341aa55415c05d42
tracer-ics2019 <tracer@njuics.org>
2025-01-04 16:40:01 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:40:01 up  1:56,  1 user,  load average: 0.24, 0.15, 0.08 5fd039c698de1507f3aea91784bdcfdea9cfa28a

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -9,7 +9,7 @@ const char *regsl[] = {
 
 void isa_reg_display() {
   for(int i = 0; i < sizeof(regsl) / sizeof(regsl[0]); i++){
-    printf("%s:%x\n", regsl[i], cpu.gpr[i]._32);
+    printf("%s:0x%8x\n", regsl[i], cpu.gpr[i]._32);
   }
 }
 
