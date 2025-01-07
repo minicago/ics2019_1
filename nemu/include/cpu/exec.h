81d93d07788efb5d11767578b4f48ee98992c6ae
tracer-ics2019 <tracer@njuics.org>
2025-01-07 16:05:20 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:05:20 up 42 min,  1 user,  load average: 0.25, 0.11, 0.07 771ef09eea5749ab1d7ebc9cb2fd0b66b81d464c

--- nemu/include/cpu/exec.h
+++ nemu/include/cpu/exec.h
@@ -38,6 +38,7 @@ static inline uint32_t instr_fetch(vaddr_t *pc, int len) {
 
 /* Instruction Decode and EXecute */
 static inline void idex(vaddr_t *pc, OpcodeEntry *e) {
+  printf("%x %x\n",e->decode, e->execute);
   if (e->decode)
     e->decode(pc);
   e->execute(pc);
