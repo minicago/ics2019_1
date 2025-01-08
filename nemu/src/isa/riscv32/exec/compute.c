5108335eab61e3fec8e4bd23dbe306eac9397628
tracer-ics2019 <tracer@njuics.org>
2025-01-08 16:24:20 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:24:20 up  8:31,  1 user,  load average: 0.22, 0.17, 1.52 64d31501de3b4f06c382ca694a9d8bb1aef438eb

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -7,7 +7,7 @@ make_EHelper(lui) {
 
 make_EHelper(auipc) {
   printf("%x\n", *pc + id_src->simm);  
-  rtl_li(id_dest, *pc + id_src->simm);
+  rtl_li(&(id_dest->reg), *pc + id_src->simm);
   printf("%x %d\n",id_dest->reg, reg_l(2));
   print_asm_template2(auipc);
 }
