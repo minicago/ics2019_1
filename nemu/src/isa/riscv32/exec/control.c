474310a7c93a15f438c6e0fcd1ea926bd48a10f4
tracer-ics2019 <tracer@njuics.org>
2025-01-09 14:42:50 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:42:50 up 1 day,  6:36,  1 user,  load average: 0.00, 0.04, 0.07 32af6046d611a44f758dd78751ef42ac62cddfb5

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -1,7 +1,7 @@
 #include "cpu/exec.h"
 make_EHelper(jal){
     // printf("jal %x\n",*pc + id_src->simm + 4);
-  rtl_li(&reg_l(id_dest->reg), pc);
+  rtl_addi(&reg_l(id_dest->reg), pc , 4);
   rtl_addi(&t0, pc, id_src->simm);
   // printf("%x\n",t0);
   rtl_j(t0);
@@ -12,6 +12,7 @@ make_EHelper(jalr){
   // printf("ok");
   rtl_addi(&reg_l(id_dest->reg), pc , 4);
   rtl_addi(&t0, &reg_l(id_src->reg), id_src2->simm);
+  rtl_addi(&t0,&t0,~1);
   rtl_j(t0);
 }
 
