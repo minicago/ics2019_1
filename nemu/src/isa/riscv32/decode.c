a3cfba2f1fa82ce764ffa92c0abf0fa6c5cfa847
tracer-ics2019 <tracer@njuics.org>
2025-01-09 15:00:46 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:00:46 up 1 day,  6:53,  1 user,  load average: 0.04, 0.32, 0.32 133dff8dc81259da93a553be3b7f0fb2749dcc4

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -54,7 +54,7 @@ make_DHelper(st) {
 
 make_DHelper(I) {
   decode_op_r(id_src, decinfo.isa.instr.rs1, true);
-  decode_op_i(id_src2, decinfo.isa.instr.simm11_0 + *pc, true);
+  decode_op_i(id_src2, decinfo.isa.instr.simm11_0, true);
   decode_op_r(id_dest, decinfo.isa.instr.rd, false);
 }
 
@@ -64,7 +64,13 @@ make_DHelper(shamt) {
 
 make_DHelper(J) {
   decode_op_i(id_src, 
-  (decinfo.isa.instr.simm20 << 20) | (decinfo.isa.instr.imm19_12 << 12) | (decinfo.isa.instr.imm19_12 << 12) | (decinfo.isa.instr.imm11_ << 11) | (decinfo.isa.instr.imm10_1 << 1),
+  (
+    (decinfo.isa.instr.simm20 << 20) 
+  | (decinfo.isa.instr.imm19_12 << 12) 
+  | (decinfo.isa.instr.imm19_12 << 12) 
+  | (decinfo.isa.instr.imm11_ << 11) 
+  | (decinfo.isa.instr.imm10_1 << 1)
+  ) + *pc,
   true);
   decode_op_r(id_dest, decinfo.isa.instr.rd, false);
 }
