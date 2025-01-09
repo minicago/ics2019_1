6b890c995538789773a640b303bd3cde2684b956
tracer-ics2019 <tracer@njuics.org>
2025-01-09 15:25:52 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:25:52 up 1 day,  7:18,  1 user,  load average: 0.15, 0.16, 0.20 9bced2e3574b99f012c996cbf9cde6167baebe0

--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -52,6 +52,6 @@ void isa_exec(vaddr_t *pc) {
   assert(decinfo.isa.instr.opcode1_0 == 0x3);
   // printf("id: %x\n",decinfo.isa.instr.opcode6_2);
   idex(pc, &opcode_table[decinfo.isa.instr.opcode6_2]);
-  // printf(" jmp?%d \n", decinfo.is_jmp);
+  printf(" jmp?%d \n", decinfo.is_jmp);
   
 }
