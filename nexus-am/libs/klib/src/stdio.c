680ebcb40643f6cd02e0621114d8dad846aa8a26
tracer-ics2019 <tracer@njuics.org>
2025-01-10 15:59:48 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:59:48 up 2 days,  7:47,  1 user,  load average: 0.27, 0.16, 0.13 3563e7c1a6eebc4d07bb9e2112371dea98e6f03

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,20 +3,102 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  return 0;
+
+static char NUM_CHAR[] = "0123456789ABCDEF";
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+    int len = 0;
+    char buf[128];
+    int buf_len = 0;
+    while(*fmt != '\0' && len < n){
+        char* s; char c; int val;uint32_t uval,address;
+        switch(*fmt) {
+            case '%':
+              fmt++;
+              switch(*fmt) {
+                case 'd':
+                  val = va_arg(ap, int);    
+                  if(val == 0) out[len++] = '0';
+                  if(val < 0) {
+                    out[len++] = '-';
+                    val = 0 - val;
+                  }
+                  for(buf_len = 0; val; val /= 10, buf_len++)
+                    buf[buf_len] = NUM_CHAR[val % 10];    
+                  for(int i = buf_len - 1; i >= 0; i--)
+                    out[len++] = buf[i];
+                break;
+                case 'u':
+                  uval = va_arg(ap, uint32_t);
+                break;
+                case 'c':
+                  c = (char)va_arg(ap, int);
+                  out[len++] = c;
+                break;
+                case 's':
+                  s = va_arg(ap, char*);
+                  for(int i = 0; s[i] != '\0'; i++)
+                    out[len++] = s[i];
+                break;
+                case 'p':
+                  out[len++] = '0'; out[len++] = 'x';
+                  address = va_arg(ap, uint32_t);
+                  for(buf_len = 0; address; address /= 16, buf_len++)
+                    buf[buf_len] = NUM_CHAR[address % 16];
+                  for(int i = buf_len - 1; i >= 0; i--)
+                    out[len++] = buf[i];
+                break;               
+              }
+            break; 
+            case '\n':
+              out[len++] = '\n';
+            break;
+            default:
+              out[len++] = *fmt;
+        }
+        fmt++;
+    }
+    out[len] = '\0';    
+    return len;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+  return vsnprintf(out, -1, fmt, ap);
 }
+
+
+int printf(const char *fmt, ...) {
+  char buffer[1024];
+  va_list arg;
+  va_start(arg, fmt);
+
+  int done = vsprintf(buffer, fmt, arg);
+  // putstr(buffer);
+
+  va_end(arg);
+  return done;
+}
+
+
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  va_list arg;
+  va_start(arg, fmt);
+
+  int res = vsprintf(out,fmt, arg);
+
+  va_end(arg);
+  return res;
 }
 
+
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
+  va_list arg;
+  va_start(arg, fmt);
+
+  int res = vsnprintf(out, n ,fmt, arg);
+
+  va_end(arg);
+  return res;
 }
 
 #endif
