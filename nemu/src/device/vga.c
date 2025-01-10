2fc30c317a510641cd639feec7c201e557282162
tracer-ics2019 <tracer@njuics.org>
2025-01-10 18:14:16 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:14:16 up  1:29,  1 user,  load average: 0.47, 0.26, 0.25 ea90f1147968a81839b990ef1021d61a8cd0ae74

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -32,7 +32,7 @@ static inline void update_screen() {
 static void vga_io_handler(uint32_t offset, int len, bool is_write) {
   // TODO: call `update_screen()` when writing to the sync register
   // TODO();
-  printf("ok");
+  printf("%d", sync_port_base[0]);
   if (sync_port_base[0]) {
     
     update_screen();
