951a5ff28d0a0a00bd8f97fa39895dc798d11c99
tracer-ics2019 <tracer@njuics.org>
2025-01-11 02:36:38 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:36:38 up  9:47,  1 user,  load average: 0.09, 0.10, 0.11 c8c44370d4c91e7053f3ca5ed07bdb925e06b98b

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -4,6 +4,9 @@ void raise_intr(uint32_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+    cpu.sepc = epc;
+    cpu.scause = 0x8;
+    rtl_jr(&cpu.stvec);  
 }
 
 bool isa_query_intr(void) {
