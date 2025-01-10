7c7de9765018f442cbc84008837deafe0088a29d
tracer-ics2019 <tracer@njuics.org>
2025-01-10 15:49:35 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:49:35 up 2 days,  7:37,  1 user,  load average: 0.07, 0.11, 0.14 392571b7f622cadaaec0e8154fc3c4b713be9ac0

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,19 +3,35 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
+  for(int i = 0; ; i++){
+    if(s[i]==0) return i;
+  }
   return 0;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  for(int i = 0; ; i++){
+    dst[i] = src[i];
+    if(src[i] == 0) break;
+  }
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  for(int i = 0; i < n; i++){
+    dst[i] = src[i];
+    if(src[i] == 0) break;
+  }
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  int dst_len = strlen(dst);
+  for(int i = 0; ; i++){
+    dst[dst_len + i] = src[i];
+    if(src[i] == 0) break;
+  }
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
@@ -23,22 +39,35 @@ int strcmp(const char* s1, const char* s2) {
     if(s1[i]==0 && s2[i]==0) return 0;
     else if(s1[i] != s2[i]) return -1;
   }
-  return -1;
+  return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
+  for (int i = 0; i < n ; i++){
+    if(s1[i]==0 && s2[i]==0) return 0;
+    else if(s1[i] != s2[i]) return -1;
+  }
   return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  for(int i=0; i<n; i++){
+    *(char*)(v+i) = c;
+  }
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  for(int i=0; i<n; i++){
+    *(char*)(out+i) = *(char*)(in+i);
+  }
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
+  for (int i = 0; i < n ; i++){
+    if(*(char*) (s1+i) !=*(char*) (s2+i) ) return -1;
+  }  
   return 0;
 }
 
