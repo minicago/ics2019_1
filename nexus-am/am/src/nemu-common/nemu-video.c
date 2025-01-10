004f4c20b498300b583a8da9ddab4c491ddbe475
tracer-ics2019 <tracer@njuics.org>
2025-01-10 18:01:05 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:01:05 up  1:16,  1 user,  load average: 0.20, 0.28, 0.27 569d496f546114262b08955a57be85562e6ea521

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -6,8 +6,8 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = 400;
-      info->height = 300;
+      info->width = inl(SCREEN_ADDR) & 0xffff;
+      info->height = ((uint32_t) inl(SCREEN_ADDR)) >> 4;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
