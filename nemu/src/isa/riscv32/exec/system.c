90e769072c4077257ee3855f26639f07f88aadbf
tracer-ics2019 <tracer@njuics.org>
2025-01-11 03:25:48 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:25:48 up 10:36,  1 user,  load average: 0.07, 0.18, 0.14 b6ce1d77e7736b7e2c88e7f89588eeb25f0f16d4

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -6,18 +6,18 @@ make_EHelper(ecall){
 
 make_EHelper(CSRRW){
     Log("%x",*CSR(id_src2->val));
-    rtl_li (id_dest , *CSR(id_src2->val));
+    rtl_li (&reg_l(id_dest)  , *CSR(id_src2->val));
     *CSR(id_src2->val) = id_src->val;
 }
 make_EHelper(CSRRS){
     Log("%x",*CSR(id_src2->val));
     
-    rtl_li (id_dest , *CSR(id_src2->val));
+    rtl_li (&reg_l(id_dest) , *CSR(id_src2->val));
     Log("%x",reg_l(id_dest->reg));
     *CSR(id_src2->val) |= id_src->val;
 }
 make_EHelper(CSRRC){
     Log("%x",*CSR(id_src2->val));
-    rtl_li (id_dest , *CSR(id_src2->val));
+    rtl_li (&reg_l(id_dest) , *CSR(id_src2->val));
     *CSR(id_src2->val) |= ~id_src->val;
 }
