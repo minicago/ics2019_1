1465eda0588ee0750847abc0ab122946398d72f2
tracer-ics2019 <tracer@njuics.org>
2025-01-11 02:09:47 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:09:47 up  9:20,  1 user,  load average: 0.19, 0.17, 0.17 f4b3c904d63ba8f3e481a13883f0bcb5ca27a971

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -1,21 +1,14 @@
 #include "cpu/exec.h"
 
 make_EHelper(CSRRW){
-    
+    rtl_li (id_dest , *CSR(id_src2->val));
+    *CSR(id_src2->val) = id_src->val;
 }
 make_EHelper(CSRRS){
-
+    rtl_li (id_dest , *CSR(id_src2->val));
+    *CSR(id_src2->val) |= id_src->val;
 }
 make_EHelper(CSRRC){
-
-}
-
-make_EHelper(CSRRWI){
-
-}
-make_EHelper(CSRRSI){
-
-}
-make_EHelper(CSRRCI){
-    
+    rtl_li (id_dest , *CSR(id_src2->val));
+    *CSR(id_src2->val) |= ~id_src->val;
 }
