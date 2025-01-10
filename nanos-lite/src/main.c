6b8588ea04a0302015856a14ea2636ce7452e429
tracer-ics2019 <tracer@njuics.org>
2025-01-11 01:07:14 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:07:14 up  8:18,  1 user,  load average: 0.31, 0.19, 0.15 a54fb23f3b1bcb5e959ebe5eefc05b093e409ed

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -10,8 +10,8 @@ void init_proc(void);
 extern const char logo[];
 
 int main() {
-  printf("%s","?");
-  // printf("%s", logo);
+  // printf("%s","?");
+  printf("%s", logo);
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
 
