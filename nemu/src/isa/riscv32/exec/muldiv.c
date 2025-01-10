42207ee53d912764fd427f9fed179bc999c7e6d0
tracer-ics2019 <tracer@njuics.org>
2025-01-10 15:16:45 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:16:45 up 2 days,  7:04,  1 user,  load average: 0.29, 0.20, 0.17 7858ac0d3fcb24c2325ec10a7042f4e9ce3818b

--- nemu/src/isa/riscv32/exec/muldiv.c
+++ nemu/src/isa/riscv32/exec/muldiv.c
@@ -7,7 +7,7 @@ make_EHelper(mul){
     print_asm_template3(mul);
 }
 make_EHelper(mulh){
-    rtl_mul_hi(R_arg);
+    rtl_imul_hi(R_arg);
     print_asm_template3(mulh);    
 }
 make_EHelper(mulhsu){
@@ -15,25 +15,25 @@ make_EHelper(mulhsu){
     print_asm_template3(mulhsu);     
 }
 make_EHelper(mulhu){
-    rtl_imul_hi(R_arg);
+    rtl_mul_hi(R_arg);
     Log("mulhu %x %x = %x", id_src->val, id_src2->val, reg_l(id_dest->reg));
     print_asm_template3(mulhu);     
 }
 
 make_EHelper(div){
-    rtl_div_q(R_arg);
+    rtl_idiv_q(R_arg);
     print_asm_template3(div);  
 }
 make_EHelper(divu){
-    rtl_idiv_q(R_arg);
+    rtl_div_q(R_arg);
     print_asm_template3(divu);  
 }
 
 make_EHelper(rem){
-    rtl_div_r(R_arg);
+    rtl_idiv_r(R_arg);
     print_asm_template3(rem);  
 }
 make_EHelper(remu){
-    rtl_idiv_r(R_arg);
+    rtl_div_r(R_arg);
     print_asm_template3(remu);  
 }
