86d49ff36d9b01b96e81b9c668703b0d1192ffc6
tracer-ics2019 <tracer@njuics.org>
2025-01-10 17:51:04 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:51:04 up  1:06,  1 user,  load average: 0.52, 0.43, 0.28 875cf2258b5a45bea10aef1d2c06045b9ef9637

--- nexus-am/tests/amtest/src/tests/rtc.c
+++ nexus-am/tests/amtest/src/tests/rtc.c
@@ -7,12 +7,12 @@ void rtc_test() {
   while (1) {
     while(uptime() < 1000 * sec) ;
     get_timeofday(&rtc);
-    // printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
-    // if (sec == 1) {
-    //   printf("%d second).\n", sec);
-    // } else {
-    //   printf("%d seconds).\n", sec);
-    // }
+    printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
+    if (sec == 1) {
+      printf("%d second).\n", sec);
+    } else {
+      printf("%d seconds).\n", sec);
+    }
     sec ++;
   }
 }
