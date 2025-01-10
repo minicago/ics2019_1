d0161f0dd4c83464d84646cca387cd68d5daa2f0
tracer-ics2019 <tracer@njuics.org>
2025-01-10 17:27:25 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:27:25 up 43 min,  1 user,  load average: 0.01, 0.16, 0.14 de9d8219ca710cf230fa47fec31e724e84f06187

--- nexus-am/tests/amtest/src/main.c
+++ nexus-am/tests/amtest/src/main.c
@@ -15,7 +15,6 @@ static const char *tests[256] = {
 };
 
 int main(const char *args) {
-  printf("ok");
   switch (args[0]) {
     CASE('h', hello);
     CASE('i', hello_intr, IOE, CTE(simple_trap));
