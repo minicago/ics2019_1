3048b39c99664d5ceea4cf35b6889ea859021b13
tracer-ics2019 <tracer@njuics.org>
2025-01-10 13:41:47 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:41:47 up 2 days,  5:29,  1 user,  load average: 0.04, 0.09, 0.09 b60372b2a4c385f3f7293d2e7ea3eb7e5bfb43d1

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -22,7 +22,7 @@ make_EHelper(auipc) {
 //     print_asm_template3(name); \
 //   }
 
-#define SEXT_(x, w) ((x) | (-(1<<((w)-1) | x)) )
+#define SEXT_(x, w) ((x) | (-(int32_t)(1<<((w)-1) | x)) )
 #define SEXT(src, w) \
   do{ \
     src->val = SEXT_(src->val, w); \
