3aa69062a4922d49b2798ef05763be9906b8a178
tracer-ics2019 <tracer@njuics.org>
2025-01-10 14:23:32 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:23:32 up 2 days,  6:11,  1 user,  load average: 0.15, 0.16, 0.11 fef6e3c3a38bfcbba1cde930293ebf8d8afdf6d6

--- nexus-am/tests/cputest/tests/if-else.c
+++ nexus-am/tests/cputest/tests/if-else.c
@@ -1,5 +1,5 @@
 #include "trap.h"
-__attribute__ ((__noinline__))
+
 int if_else(int n) {
 	int cost;
 	if(n > 500) cost = 150;
@@ -14,8 +14,8 @@ int if_else(int n) {
 int test_data[] = {-1, 0, 49, 50, 51, 99, 100, 101, 299, 300, 301, 499, 500, 501};
 int ans[] = {0, 0, 0, 0, 50, 50, 50, 75, 75, 75, 100, 100, 100, 150};
 
-// #define NR_DATA (sizeof(test_data) / sizeof(test_data[0])) 
-#define NR_DATA 1
+#define NR_DATA (sizeof(test_data) / sizeof(test_data[0])) 
+
 
 int main() {
 	int i, ans_idx = 0;
