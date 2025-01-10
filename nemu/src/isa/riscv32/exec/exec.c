c3b87ec2fe8a07e2947d34a6150d45fc33cdbec9
tracer-ics2019 <tracer@njuics.org>
2025-01-10 14:11:15 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:11:15 up 2 days,  5:58,  1 user,  load average: 0.32, 0.16, 0.11 57f8554fa698e0a87f262c3551e1bea6c092eda

--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -58,6 +58,9 @@ void isa_exec(vaddr_t *pc) {
   if(*pc==0x80100054) {
     printf("if-else %x %x\n", reg_l(10), reg_l(10));
   }    
+  if(*pc==0x80100058) {
+    printf("if-else %x %x\n", reg_l(10), reg_l(10));
+  }      
   idex(pc, &opcode_table[decinfo.isa.instr.opcode6_2]);
   
 
