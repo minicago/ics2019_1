bdb2989692e8617865310b580a72db460ea2c1a9
tracer-ics2019 <tracer@njuics.org>
2025-01-12 02:21:02 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:21:02 up  1:45,  1 user,  load average: 0.28, 0.16, 0.14 b08aa09ec8012bc941be6a72a367d4d976c64c2b

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -4,7 +4,7 @@ make_EHelper(ecall){
     if(decinfo.isa.instr.rs2 == 0){
          raise_intr(reg_l(17), *pc);
     } else {
-        Log("%x",cpu.sepc);
+        // Log("%x",cpu.sepc);
         rtl_jr(&cpu.sepc);
     }
    
