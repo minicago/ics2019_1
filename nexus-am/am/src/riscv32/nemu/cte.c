5b3472b76566462539e3a579507046367db85ab4
tracer-ics2019 <tracer@njuics.org>
2025-01-12 02:28:43 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:28:43 up  1:53,  1 user,  load average: 0.57, 0.33, 0.19 c6d9c00481237145ee020db4d4f2ddea6b02e7

--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -18,7 +18,7 @@ _Context* __am_irq_handle(_Context *c) {
         ev.event = _EVENT_YIELD;
         c->epc += 4;
         break;
-      case 1:
+      case 0: case 1:
         ev.event = _EVENT_SYSCALL;
         c->epc += 4;
         break;
