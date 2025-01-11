f0b3da2a99cb3bad2397c8bb15e11d309740c0af
tracer-ics2019 <tracer@njuics.org>
2025-01-12 01:39:56 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:39:56 up  1:04,  1 user,  load average: 0.17, 0.22, 0.24 87f05cc241995082363b78b13aff6c61600806bf

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -4,7 +4,5 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-  int a1=0;
-  // printf("%d\n",a1);
-  return _syscall_(SYS_yield, a1, 0, 0);
+  return _syscall_(SYS_yield, 0, 0, 0);
 }
