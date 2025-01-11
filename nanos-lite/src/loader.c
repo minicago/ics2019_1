ea2988e6773520a40e9dc3f457a5b3d4656b7b17
tracer-ics2019 <tracer@njuics.org>
2025-01-12 00:49:01 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:49:01 up 14 min,  1 user,  load average: 0.00, 0.00, 0.00 6fadbc973b728a5e385120fc2eaafe89cf56e4da

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,7 +10,22 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  // TODO();
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  // check valid elf
+  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
+
+  Elf_Phdr phdr[ehdr.e_phnum];
+  ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
+  for (int i = 0; i < ehdr.e_phnum; i++) {
+    if (phdr[i].p_type == PT_LOAD) {
+      ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+      // set .bss with zeros
+      memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+    }
+  }
+  return ehdr.e_entry;
   return 0;
 }
 
