df62c3a485b9ec5bb6916d0924b380e9b093c24b
tracer-ics2019 <tracer@njuics.org>
2025-01-12 01:34:06 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:34:06 up 59 min,  1 user,  load average: 0.12, 0.20, 0.22 32691b60aca7d836ccdc92df36cfdff2dc3af322

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,23 +10,24 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  // TODO();
+  // open elf
   Elf_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
-  // check valid elf
-  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
+  assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
 
-  Elf_Phdr phdr[ehdr.e_phnum];
-  ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
+  // check elf type
+  // assert(ehdr.e_machine == );
+
+  // read phdr
+  Elf_Phdr ph[ehdr.e_phnum];
+  ramdisk_read(ph, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
   for (int i = 0; i < ehdr.e_phnum; i++) {
-    if (phdr[i].p_type == PT_LOAD) {
-      ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
-      // set .bss with zeros
-      memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+    if (ph[i].p_type == PT_LOAD) {
+      ramdisk_read((void *)ph[i].p_vaddr, ph[i].p_offset, ph[i].p_memsz);
+      memset((void *)(ph[i].p_vaddr + ph[i].p_filesz), 0, ph[i].p_memsz - ph[i].p_filesz);
     }
   }
   return ehdr.e_entry;
-  return 0;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
