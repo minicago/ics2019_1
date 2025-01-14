2186e495b524075eaaa3f379bd6b0190d43d4afc
tracer-ics2019 <tracer@njuics.org>
2025-01-14 19:53:40 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:53:40 up  3:18,  1 user,  load average: 0.69, 0.27, 0.16 538473028ee8df108ec6594b5c9b8021617f6010

--- navy-apps/tests/events/events.c
+++ navy-apps/tests/events/events.c
@@ -9,7 +9,7 @@ int main() {
     char buf[256];
     char *p = buf, ch;
     while ((ch = fgetc(fp)) != -1) {
-      printf("%x\n", ch);
+      printf("%x %x\n", ch, ch == -1);
       // return 0;
       *p ++ = ch;
       if(ch == '\n') {
