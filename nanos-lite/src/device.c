41c509c4395e0033643bd7964ad6c58356c0bdcd
tracer-ics2019 <tracer@njuics.org>
2025-01-15 00:45:10 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:45:10 up 25 min,  1 user,  load average: 0.09, 0.11, 0.09 6bab7df0ca7497492ea0416bb0bbe08863899801

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -52,15 +52,28 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+return snprintf((char *)buf, len, "WIDTH:%d\nHEIGHT:%d\n", screen_width(), screen_height());
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return len;
-
+  uint32_t* pixels = (uint32_t *)buf;
+  uint32_t x = offset % screen_width();
+  uint32_t y = offset / screen_width();
+  uint32_t w = len, h = 1;
+  
+  draw_rect(pixels, x, y, w, h);
+  draw_sync();
+  return 0;
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
+  uint32_t* pixels = (uint32_t *)buf;
+  uint32_t x = offset % screen_width();
+  uint32_t y = offset / screen_width();
+  uint32_t w = len, h = 1;
+  
+  draw_rect(pixels, x, y, w, h);
+  draw_sync();  
   return 0;
 }
 
