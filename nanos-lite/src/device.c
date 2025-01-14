d683fc1d38c3af5313592834c79e50148418f8c9
tracer-ics2019 <tracer@njuics.org>
2025-01-15 01:15:35 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:15:35 up 55 min,  1 user,  load average: 1.41, 0.62, 0.39 142cdd47a4f16cf138e2ccbd32f40f38628a85eb

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -68,12 +68,6 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
-  uint32_t* pixels = (uint32_t *)buf;
-  uint32_t x = offset % screen_width();
-  uint32_t y = offset / screen_width();
-  uint32_t w = len, h = 1;
-  
-  draw_rect(pixels, x, y, w, h);
   draw_sync();  
   return 0;
 }
