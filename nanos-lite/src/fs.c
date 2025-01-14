b2f95f3eae54c6f8c800d7a00aed3fa30b20d7c0
tracer-ics2019 <tracer@njuics.org>
2025-01-14 19:01:46 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:01:46 up  2:31,  1 user,  load average: 0.36, 0.14, 0.04 a84005a2a77b727a5b03fc79841f5dc61569fe

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -51,7 +51,7 @@ int fs_open(const char *pathname, int flags, int mode) {
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {
-  if (fd < 3) {
+  if (file_table[fd].read) {
     return file_table[fd].read(fd, buf, len);
   }
   
@@ -71,14 +71,8 @@ size_t fs_read(int fd, void *buf, size_t len) {
 
 size_t fs_write(int fd, const void *buf, size_t len) {
   size_t ret = 0;
-  if (fd == 1 || fd == 2) {
-    for (int i = 0; i < len; i++) {
-      _putc(*((char*)(buf) + i));
-    }
-    ret = len;
-  }
-  else if (fd == 0) {
-      return 0;
+  if (file_table[fd].write) {
+    return file_table[fd].write(fd, buf, len);
   }
   else {
     assert(file_table[fd].open_offset + len <= file_table[fd].size);
