cdb901514d05f4431b3b0f5a734ff9970ca12791
tracer-ics2019 <tracer@njuics.org>
2025-01-15 00:32:57 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:32:57 up 13 min,  1 user,  load average: 0.22, 0.16, 0.10 9eedcec9c11e9ee5f42e5d27881ddfbe5639f997

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -2,6 +2,7 @@
 #include <amdev.h>
 
 #include <am.h>
+// #include <ndl.h>
 
 #define KEYDOWN_MASK 0x8000
 
@@ -54,17 +55,19 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  return len;
+
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
-
+int fs_open(const char *pathname, int flags, int mode);
 void init_device() {
   Log("Initializing devices...");
   _ioe_init();
-
+  int fd = fs_open("/dev/fb", 0, 0);
+  
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
 }
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -27,11 +27,16 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 size_t serial_write(const void *buf, size_t offset, size_t len);
 size_t events_read(void *buf, size_t offset, size_t len);
 /* This is the information about all files in disk. */
+
+size_t dispinfo_read(void *buf, size_t offset, size_t len);
+size_t fb_write(const void *buf, size_t offset, size_t len);
+
 static Finfo file_table[] __attribute__((used)) = {
-  {"stdin", 0, 0, 0, invalid_read, invalid_write},
-  {"stdout", 0, 0, 0, invalid_read, serial_write},
-  {"stderr", 0, 0, 0, invalid_read, serial_write},
+  [FD_STDIN]{"stdin", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT]{"stdout", 0, 0, 0, invalid_read, serial_write},
+  [FD_STDERR]{"stderr", 0, 0, 0, invalid_read, serial_write},
   {"/dev/events", 0 ,0, 0, events_read,invalid_write},
+  [FD_FB]{"/dev/fb", 0, 0, 0, dispinfo_read, fb_write},
 #include "files.h"
 };
 
@@ -56,7 +61,6 @@ int fs_open(const char *pathname, int flags, int mode) {
 size_t fs_read(int fd, void *buf, size_t len) {
   
   if (file_table[fd].read) {
-    // Log("%x %x",fd, file_table[fd].read);
     return file_table[fd].read(buf, file_table[fd].open_offset , len);
   }
   
