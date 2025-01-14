c81558d14e5cb4206838ce3c27773fcb9a1adbd1
tracer-ics2019 <tracer@njuics.org>
2025-01-14 19:20:22 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:20:22 up  2:48,  1 user,  load average: 0.00, 0.17, 0.21 cc715cb9a1e5b0699fb430d7221f62507d753b60

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,6 +1,15 @@
 #include "common.h"
 #include <amdev.h>
 
+#include <am.h>
+
+#define KEYDOWN_MASK 0x8000
+
+#define KEY_QUEUE_LEN 1024
+static int key_queue[KEY_QUEUE_LEN] = {};
+static int key_f = 0, key_r = 0;
+// static SDL_mutex *key_queue_lock = NULL;
+
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   char *cbuf = (char *)buf;
   // Log("%x", cbuf);
@@ -20,7 +29,20 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
+  int k = _KEY_NONE;
+
+  // SDL_LockMutex(key_queue_lock);
+  if (key_f != key_r) {
+    k = key_queue[key_f];
+    key_f = (key_f + 1) % KEY_QUEUE_LEN;
+  }
+  // SDL_UnlockMutex(key_queue_lock);
+
+  kbd->keydown = (k & KEYDOWN_MASK ? 1 : 0);
+  kbd->keycode = k & ~KEYDOWN_MASK;
+
+  return sizeof(_DEV_INPUT_KBD_t);
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
