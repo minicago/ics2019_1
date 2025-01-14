a2904747f9dd4b87dddbc57ab59dae199886a215
tracer-ics2019 <tracer@njuics.org>
2025-01-15 02:02:10 +0800

    >  compile U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:02:10 up  1:42,  1 user,  load average: 0.15, 0.37, 0.40 706bbe56a035c199db6d11a351707e1a6174251e

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -37,7 +37,8 @@ char* strcat(char* dst, const char* src) {
 int strcmp(const char* s1, const char* s2) {
   for (int i = 0; ; i++){
     if(s1[i]==0 && s2[i]==0) return 0;
-    else if(s1[i] != s2[i]) return -1;
+    else if(s1[i] < s2[i]) return -1;
+    else if(s1[i] > s2[i]) return 1;
   }
   return 0;
 }
