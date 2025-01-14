315da95dee9de6b23eee3c823e3bac0ff5f98410
tracer-ics2019 <tracer@njuics.org>
2025-01-14 19:29:14 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:29:14 up  2:56,  1 user,  load average: 0.19, 0.11, 0.16 ee0514cf2ae7d1f5943defb72b629d605997f5d3

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -31,7 +31,7 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, 0, invalid_read, invalid_write},
   {"stdout", 0, 0, 0, invalid_read, serial_write},
   {"stderr", 0, 0, 0, invalid_read, serial_write},
-  {"/dev/event", 0 ,0, events_read,invalid_write},
+  {"/dev/events", 0 ,0, events_read,invalid_write},
 #include "files.h"
 };
 
