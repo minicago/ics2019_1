565230764027580c543332a64fea1c4d16d5254b
tracer-ics2019 <tracer@njuics.org>
2025-01-14 19:47:03 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:47:03 up  3:12,  1 user,  load average: 0.12, 0.09, 0.12 40be6bafe122113df0a3524048b84d118c405ff3

--- navy-apps/tests/events/events.c
+++ navy-apps/tests/events/events.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  printf("Start to receive events...\n");
+  
   FILE *fp = fopen("/dev/events", "r");
   int time = 0;
-  
+  printf("Start to receive events...\n");
   while (1) {
     char buf[256];
     char *p = buf, ch;
     while ((ch = fgetc(fp)) != -1) {
+      printf("Start to receive events...\n");
       *p ++ = ch;
       if(ch == '\n') {
         *p = '\0';
