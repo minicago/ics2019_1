68839517b73e62d7f556facaca021d568a3de7a3
tracer-ics2019 <tracer@njuics.org>
2025-01-15 01:05:26 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:05:26 up 45 min,  1 user,  load average: 0.13, 0.18, 0.17 36e2ae7641a5ce2ad1b24dd33da3b46657e031e

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -52,7 +52,8 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return strncpy(buf, dispinfo + offset, len);
+  strncpy(buf, dispinfo + offset, len);
+  return len;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
@@ -83,7 +84,6 @@ void init_device() {
   Log("Initializing devices...");
   _ioe_init();
   sprintf((char *)dispinfo, "WIDTH:%d\nHEIGHT:%d\n", screen_width(), screen_height());
-  // printf(dispinfo);
   
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
