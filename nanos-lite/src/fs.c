20ba096f3ca523635ce661a13f8f6bae3dce7522
tracer-ics2019 <tracer@njuics.org>
2025-01-15 01:14:01 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:14:01 up 54 min,  1 user,  load average: 0.40, 0.38, 0.31 931827c3b2991623d6fd86887b3f28e53f4d9ec

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -64,7 +64,9 @@ int fs_open(const char *pathname, int flags, int mode) {
 size_t fs_read(int fd, void *buf, size_t len) {
   
   if (file_table[fd].read) {
-    return file_table[fd].read(buf, file_table[fd].open_offset , len);
+    size_t ret = file_table[fd].read(buf, file_table[fd].open_offset , len);
+    file_table[fd].open_offset += ret;
+    return ret;
   }
   
   size_t size = file_table[fd].size;
@@ -85,7 +87,9 @@ size_t fs_write(int fd, const void *buf, size_t len) {
   
   size_t ret = 0;
   if (file_table[fd].write) {
-    return file_table[fd].write(buf, file_table[fd].open_offset , len);
+        size_t ret = file_table[fd].write(buf, file_table[fd].open_offset , len);
+    file_table[fd].open_offset += ret;
+    return ret;
   } else {
     assert(file_table[fd].open_offset + len <= file_table[fd].size);
     ret = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
