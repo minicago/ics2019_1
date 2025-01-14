7c527acb472ffceee488c48603ea51ad68141e0e
tracer-ics2019 <tracer@njuics.org>
2025-01-15 00:57:28 +0800

    >  run U202110780 kaka Linux KAKA 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:57:28 up 37 min,  1 user,  load average: 0.28, 0.20, 0.17 57dd6fde7fc34d49538b7df4c6776be9c500fe09

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -135,7 +135,7 @@ static void get_display_info() {
   char buf[128], key[128], value[128], *delim;
   while (fgets(buf, 128, dispinfo)) {
     *(delim = strchr(buf, ':')) = '\0';
-    printf(dispinfo);
+    printf("%s\n",buf);
     sscanf(buf, "%s", key);
     sscanf(delim + 1, "%s", value);
     if (strcmp(key, "WIDTH") == 0) sscanf(value, "%d", &screen_w);
